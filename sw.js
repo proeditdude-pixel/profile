8a0fe06d2357d3bb42aaf19b0cd8a50a541e303b
realyoking <proeditdude@gmail.com>
2026-01-17 21:09:24 +0800

    Add service worker with install, activate, and fetch events

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+    // Required for PWA (Add to Home Screen) support
+});
